844fa3fbd4d178b4092df2cc9885bf38c69cd2b4
Gaurav-0301 <gauravkakpure2006@gmail.com>
2026-03-21 19:17:31 +0530

    server js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const express= require( "express");
-const dotenv =require("dotenv");
-const todoRoutes =require("./routes/todo.route.js");
-const { connectDB } =require("./config/db.js");
-const cors= require( "cors");
-const path =require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const todoRoutes = require("./routes/todo.route.js");
+const { connectDB } = require("./config/db.js");
+const cors = require("cors");
+const path = require("path");
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -15,8 +16,6 @@ app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
@@ -26,5 +25,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
